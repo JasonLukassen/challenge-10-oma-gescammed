c27c9dda16fc1f1735a256fcc355536be625d3d4
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2023-04-20 14:31:42 +0200

    update
    Login page created.
    Login page is done except for the navigation bar.

---
+++ frontend/javascript/login.js
@@ -0,0 +1,11 @@
+
+let showPassword= () => {
+    let checkbox = document.getElementById("toggle-password-visibility");
+    let password = document.getElementById("password");
+    if (checkbox.checked === true){
+        password.type = "text";
+    }
+    else{
+        password.type = "password";
+    }
+}
